cd4e58e1caf53d736f1993c4f715b1069ec2b6e1
vinicius <vini46.job@outlook.com>
2022-11-17 13:49:41 -0300

    TecladoBateria

---
+++ javascript/aluramidi-curso-arquivos-iniciais/README.md
@@ -0,0 +1,15 @@
+# Sons de Bateria
+
+![preview](/javascript/aluramidi-curso-arquivos-iniciais/images/Teclado%20Bateria.jpeg)
+
+Este projeto apresenta uma série de butões com sons de bateria. Desenvolvido no curso da Alura para praticas boas práticas com o Javascript
+
+[Clique aqui para acessar]https://vinidevit.github.io/TecladoBateria/)
+
+# Tecnologias
+
+> HTML
+
+> CSS
+
+> Javascript
---
+++ javascript/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,22 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); 
+
+//for
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento)
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function ()  {
+        tocaSom(idAudio);
+    }
+    
+
+}
